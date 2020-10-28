008b60d8e3a54fd576545fc75d2c8223123235d8
Ervalsa Dwi Nanda <34961795+wival08@users.noreply.github.com>
2020-10-28 20:15:12 +0800

    add attack function

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 from classes.game import Person, Colors
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 10, "dmg": 80},
+         {"name": "Thunder", "cost": 12, "dmg": 80},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i = 0
+
+print(Colors.fail + Colors.bold + "\n\t\t\t\t\tAN ENEMY ATTACKS!" + Colors.endc)
+
+while running:
+    print("==========================================================")
+    player.choose_action()
+    choice = int(input("Choose action : "))
+    index = int(choice) - 1
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You attacked for", dmg, "points of damage. Enemy HP : ", enemy.get_hp())
+
+    enemy_choice = 1
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for", enemy_dmg, "points of damage. Player HP : ", player.get_hp())
+
+
+
+
+
 
